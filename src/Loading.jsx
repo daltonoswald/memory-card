43ff40ba95c87ac0f2a56c4a5182e0ffbc08f9eb
daltonoswald <daltonoswald@gmail.com>
2025-01-11 12:22:37 -0500

    button css changes and scroll bar removal

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -2,9 +2,9 @@ import pokeball from '/src/assets/pokeball.png'
 
 export default function Loading() {
     return (
-        <>
+        <div className='loading-container'>
             <h1 className="loading">Catching Pokemon...</h1>
             <img className="pokeball-img" src={pokeball} />
-        </>
+        </div>
     )
 }
